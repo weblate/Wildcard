d28c3fc464b43c4fbe7e1eba076ad8b2ea0f6652
FineFindus <FineFindus@proton.me>
2023-06-25 13:44:22 +0200

    refactor(flags): use bindings

--- src/flags_dialog.rs
+++ src/flags_dialog.rs
@@ -55,7 +55,6 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             klass.bind_template();
-            klass.bind_template_callbacks();
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -83,69 +82,6 @@ mod imp {
         }
     }
 
-    #[gtk::template_callbacks]
-    impl FlagsDialog {
-        #[template_callback]
-        fn on_multiline_changed(&self) {
-            if let Err(err) = self
-                .obj()
-                .set_flag_setting("multiline-flag", self.multiline_switch.is_active())
-            {
-                log::error!("Failed to save multiline flag, {}", &err);
-            }
-        }
-
-        #[template_callback]
-        fn on_case_insensitive_changed(&self) {
-            if let Err(err) = self.obj().set_flag_setting(
-                "case-insensitive-flag",
-                self.case_insensitive_switch.is_active(),
-            ) {
-                log::error!("Failed to save case insensitive flag, {}", &err);
-            }
-        }
-
-        #[template_callback]
-        fn on_ignore_whitespace_changed(&self) {
-            if let Err(err) = self.obj().set_flag_setting(
-                "ignore-whitespace-flag",
-                self.ignore_whitespace_switch.is_active(),
-            ) {
-                log::error!("Failed to save ignore whitespace flag, {}", &err);
-            }
-        }
-
-        #[template_callback]
-        fn on_dot_matches_newline_changed(&self) {
-            if let Err(err) = self.obj().set_flag_setting(
-                "dot-matches-newline-flag",
-                self.dot_matches_newline_switch.is_active(),
-            ) {
-                log::error!("Failed to save dot matches newline flag, {}", &err);
-            }
-        }
-
-        #[template_callback]
-        fn on_unicode_changed(&self) {
-            if let Err(err) = self
-                .obj()
-                .set_flag_setting("unicode-flag", self.unicode_switch.is_active())
-            {
-                log::error!("Failed to save unicode flag, {}", &err);
-            }
-        }
-
-        #[template_callback]
-        fn on_greed_changed(&self) {
-            if let Err(err) = self
-                .obj()
-                .set_flag_setting("greed-flag", self.greed_switch.is_active())
-            {
-                log::error!("Failed to save greed flag, {}", &err);
-            }
-        }
-    }
-
     impl WidgetImpl for FlagsDialog {}
     impl WindowImpl for FlagsDialog {}
     impl AdwWindowImpl for FlagsDialog {}
@@ -157,7 +93,7 @@ glib::wrapper! {
 }
 
 impl FlagsDialog {
-    #[warn(clippy::new_without_default)]
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         glib::Object::builder().build()
     }
@@ -165,30 +101,23 @@ impl FlagsDialog {
     fn load_flags(&self) {
         let imp = self.imp();
 
-        imp.multiline_switch
-            .set_active(imp.settings.boolean("multiline-flag"));
-        imp.case_insensitive_switch
-            .set_active(imp.settings.boolean("case-insensitive-flag"));
-        imp.ignore_whitespace_switch
-            .set_active(imp.settings.boolean("ignore-whitespace-flag"));
-        imp.dot_matches_newline_switch
-            .set_active(imp.settings.boolean("dot-matches-newline-flag"));
-        imp.unicode_switch
-            .set_active(imp.settings.boolean("unicode-flag"));
-        imp.greed_switch
-            .set_active(imp.settings.boolean("greed-flag"));
+        self.bind_flag("multiline-flag", &imp.multiline_switch);
+        self.bind_flag("case-insensitive-flag", &imp.case_insensitive_switch);
+        self.bind_flag("ignore-whitespace-flag", &imp.ignore_whitespace_switch);
+        self.bind_flag("dot-matches-newline-flag", &imp.dot_matches_newline_switch);
+        self.bind_flag("unicode-flag", &imp.unicode_switch);
+        self.bind_flag("greed-flag", &imp.greed_switch);
     }
 
-    fn set_flag_setting(
-        &self,
-        setting_name: &str,
-        setting_value: bool,
-    ) -> Result<(), glib::BoolError> {
+    fn bind_flag(&self, setting_name: &str, switch: &TemplateChild<gtk::Switch>) {
         let imp = self.imp();
 
-        imp.settings.set_boolean(setting_name, setting_value)?;
-        self.emit_by_name::<()>("flags-changed", &[]);
-
-        Ok(())
+        imp.settings.bind(setting_name, &**switch, "active").build();
+        imp.settings.connect_changed(
+            Some(setting_name),
+            glib::clone!(@weak self as dialog => move |_setting, _value| {
+                dialog.emit_by_name::<()>("flags-changed", &[]);
+            }),
+        );
     }
 }
